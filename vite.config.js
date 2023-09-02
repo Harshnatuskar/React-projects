dcee46bc2fabfa1c39a2fab4eb38b30ab7db4aa1
harshnatuskar <harshnatuskar10@gmail.com>
2023-09-02 21:05:19 +0530

    vite plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginGhPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),  
+    VitePluginGhPages({
+      base: 'quote-generator',
+      outDir: 'dist',
+    }),
+  ],
+});
